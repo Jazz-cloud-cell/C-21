f26ea3e50cf60ebab494ceda3d7eed84cffa749f
Jazz-cloud-cell <82362155+Jazz-cloud-cell@users.noreply.github.com>
2021-07-01 19:09:09 +0530

    Add files via upload

---
+++ Platform.js
@@ -0,0 +1,20 @@
+// Creating platform class, objects will be created according to posX.
+
+class Platform {
+    constructor(posX) {
+     
+      this.rx = posX;
+      this.ry = height; 
+      this.rw = random(100, 500);
+      this.rh = random([280,160]); 
+
+      this.spt=createSprite(this.rx, this.ry , this.rw, this.rh);
+      this.spt.shapeColor="green";
+      this.spt.debug =true;
+      
+    }
+  
+  
+  }
+  
+  
---
+++ sketch.js
@@ -0,0 +1,19 @@
+var platform1;
+var countDistanceX= 0;
+function preload()
+{}
+
+function setup() {
+  createCanvas(9000, 668);
+  for (i = 0; i < 20; i += 1){
+    platform1=new Platform(countDistanceX);
+    var gap=random([0,0,0,80])
+    countDistanceX = countDistanceX + platform1.rw + gap
+  }
+}
+
+function draw() {
+  background('skyblue'); 
+ drawSprites();
+}
+
